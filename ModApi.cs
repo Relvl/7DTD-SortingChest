cdd1c7f6bd804fbd704e8f72571ad160a11b8ec0
Johnson <johnssoft@ya.ru>
2026-02-15 15:56:15 +0500

    missing mod init

---
+++ ModApi.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+
+namespace SortingChest;
+
+[SuppressMessage("ReSharper", "UnusedType.Global")]
+public class ModApi : IModApi
+{
+    public void InitMod(Mod modInstance)
+    {
+        var harmony = new Harmony("sorting.chest.harmony");
+        harmony.PatchAll();
+        Log.Out($"[SortingChest] ModAPI initialized");
+    }
+}
